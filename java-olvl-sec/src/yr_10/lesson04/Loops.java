5ed97f062aca535721698fba774f38c5c4af2452
erikacamilleri <erika.camilleri@gmail.com>
2021-11-23 11:45:42 +0100

    Looping lesson

--- java-olvl-sec/src/yr_10/lesson04/Loops.java
+++ java-olvl-sec/src/yr_10/lesson04/Loops.java
@@ -8,12 +8,25 @@ public class Loops {
      * Today, we are going to learn about for loops!
      */
     public static void lesson() {
-
         // TODO Draw a square of stars using a for loop
+        System.out.println("Let's draw a rectangle using a for loop.");
+        for(int i = 1; i <= 3; i++) {
+            System.out.println("* * * * *");
+        }
 
         // TODO Print out all the numbers between 1 and 10
+        System.out.println("Printing out all the numbers between 1 and 10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
 
         // TODO Print out the three times table, and for each one print out a line of stars!
+        System.out.println("Let us do the three times table...");
+        for (int i = 1; i <= 12; i++) {
+            int timesRow = 3 * i;
+            System.out.println(i + " x 3 = " + timesRow);
+        }
+
     }
 
     /**

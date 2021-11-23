ebe11535ce50111f0607c463ed2366d52b9b60ba
erikacamilleri <erika.camilleri@gmail.com>
2021-11-23 10:44:29 +0100

    Template for lesson

---
+++ java-olvl-sec/src/yr_10/lesson04/Loops.java
@@ -0,0 +1,27 @@
+package yr_10.lesson04;
+
+import java.util.Scanner;
+
+public class Loops {
+
+    /**
+     * Today, we are going to learn about for loops!
+     */
+    public static void lesson() {
+
+        // TODO Draw a square of stars using a for loop
+
+        // TODO Print out all the numbers between 1 and 10
+
+        // TODO Print out the three times table, and for each one print out a line of stars!
+    }
+
+    /**
+     * Replace this comment with the description of the challenge that you chose!
+     */
+    public static void challenge() {
+
+        // Do not forget to import java.util.Scanner; at the very top!
+        Scanner sc = new Scanner(System.in);
+    }
+}

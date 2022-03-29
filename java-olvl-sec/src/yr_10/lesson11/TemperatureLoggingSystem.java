02f0d5a13a0a8f0c870baa6c12ba87bccd061063
erikacamilleri <erika.camilleri@gmail.com>
2022-03-29 13:01:14 +0200

    We completed the TemperatureLoggingSystem

--- java-olvl-sec/src/yr_10/lesson11/TemperatureLoggingSystem.java
+++ java-olvl-sec/src/yr_10/lesson11/TemperatureLoggingSystem.java
@@ -11,6 +11,10 @@ public class TemperatureLoggingSystem {
         // Declare variable to store user's choice
         int choice = 0;
 
+        // Declare an array to store the temperatures of last week
+        // We must do this outside of the loop otherwise every time the data gets cleared!
+        double[] temperatures = new double[7]; // 7 because 7 days of the week
+
         do {
             // Displaying the menu
             System.out.println("*** Welcome, use this program to log the temperatures of last week! ***");
@@ -25,34 +29,47 @@ public class TemperatureLoggingSystem {
             System.out.print("Please type in your choice: ");  
             choice = sc.nextInt();
 
-            // Declare an array to store the temperatures of last week
-            double[] temperatures = new double[7]; // 7 because 7 days of the week
-
             // Depending on the choice run the appropriate method
             switch (choice)
             {   
                 case 1: {
                     // Guide the user to take appropriate steps
                     System.out.println("Your choice is: Log temperatures of last week.");
-                    // TODO: Use a for loop to allow the user to insert double values in the temperatures array
+                    String[] daysOfWeek = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+                    for(int i = 0; i < 7; i++) {
+                        System.out.print("Log temperature for " + daysOfWeek[i] + " ");
+                        temperatures[i] = sc.nextDouble();
+                    }
                     break;
                 }
                 case 2: {
                     // Guide the user to take appropriate steps
                     System.out.println("Your choice is: Display all the temperatures entered.");
-                    // TODO: call method displayTempatures() and pass in the temperatures array
+                    displayTemperatures(temperatures);                    
                     break;
                 }
                 case 3: {
-                    // TODO: Display a message to the user to confirm the choice
-                    // TODO: Declare a double variable named coldest and set it to the method call.
-                    //       Do not forget to pass in the temperatures array
+                    // Display a message to the user to confirm the choice
+                    System.out.println("Your choice is: Find the coldest day");
+                    // Declare a double variable named coldest and set it to the method call.
+                    double coldest = findColdestDay(temperatures);
+                    System.out.println("The coldest day was at the temperature: " + coldest);
                     break;
                 }
                 case 4: {
+                    // Display a message to the user to confirm the choice
+                    System.out.println("Your choice is: Find the warmest day");
+                    // Declare a double variable named coldest and set it to the method call.
+                    double warmest = findWarmestDay(temperatures);
+                    System.out.println("The warmest day was at the temperature: " + warmest);
                     break;
                 }
                 case 5: {
+                    // Display a message to the user to confirm the choice
+                    System.out.println("Your choice is: Calculate the average temperature.");
+                    // Declare a double variable named coldest and set it to the method call.
+                    double average = calculateAverageTemperature(temperatures);
+                    System.out.println("The average is: " + average);
                     break;
                 }
                 case 7: {
@@ -91,10 +108,20 @@ public class TemperatureLoggingSystem {
 
     public static double findWarmestDay(double[] temperatures) {
         double warmest = temperatures[0];
-        // TODO: Complete this code to find the warmest day.
-        //       Hint, you can look at the code in findColdestDay to help you
+        for(int i = 1; i < temperatures.length; i++) {
+            if (temperatures[i] > warmest) {
+                warmest = temperatures[i];
+            }
+        }
         return warmest;
     }
 
-    // TODO: Write the method for calculateAverageTemperature
+    // The method for calculateAverageTemperature
+    public static double calculateAverageTemperature(double[] temperatures) {
+        double sum = 0;
+        for(int i=0; i < temperatures.length; i++) {
+            sum = sum + temperatures[i];
+        }
+        return (sum/temperatures.length);
+    }
 }

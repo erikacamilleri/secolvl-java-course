08f710e25c08435d078b5f12b2fa5b9111c8d288
erikacamilleri <erika.camilleri@gmail.com>
2022-05-03 10:10:05 +0200

    Add unit tests for ArrayHelpers

--- java-olvl-sec/src/yr_10/lesson12/ArrayHelpersTest.java
+++ java-olvl-sec/src/yr_10/lesson12/ArrayHelpersTest.java
@@ -1,26 +1,18 @@
 package yr_10.lesson12;
 
-import java.util.Arrays;
-
-/**
- * We will fill this in when we do Testing.
- */
 public class ArrayHelpersTest {
-
     public static void main(String[] args) {
-        testSortIntArray();
+        testIsAllSameValues();
     }
 
-    public static void testSortIntArray() {
-        int[] testData = {5, 3, 7, 4, 2};
-        int[] expectedOutput = {2, 3, 4, 5, 7};
-
-        int[] actualOutput = ArrayHelpers.sort(testData);
-
-        if (Arrays.equals(actualOutput, expectedOutput)) {
-            System.out.print("Pass ");
-        } else {
-            System.out.print("Fail ");
-        }
+    public static void testIsAllSameValues() {
+         int[] testData = {2, 2, 2, 2};
+         boolean expectedOutcome = true;
+         boolean actualOutcome = ArrayHelpers.isAllSameValues(testData, 2);
+         if (expectedOutcome == actualOutcome) {
+             System.out.print("Pass");
+         } else {
+             System.out.print("Fail");
+         }   
     }
 }

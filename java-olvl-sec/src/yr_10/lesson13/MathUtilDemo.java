4f3f5b36301eb9daaef5c794157f1977d336dfc8
erikacamilleri <erika.camilleri@gmail.com>
2022-05-17 10:16:24 +0200

    Lesson 13 examples using Math class

--- java-olvl-sec/src/yr_10/lesson13/MathUtilDemo.java
+++ java-olvl-sec/src/yr_10/lesson13/MathUtilDemo.java
@@ -1,7 +1,12 @@
 package yr_10.lesson13;
 
 public class MathUtilDemo {
-
     public static void main(String[] args) {
+        // Try out min and max of Math class
+        System.out.println("Trying out Math.min(5, 10): " + Math.min(5, 10));
+
+        System.out.println("Trying out Math.abs(-4): " + Math.abs(-4));
     }
 }
+
+
